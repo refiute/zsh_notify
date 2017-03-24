6f0cd1d54a169d299934a14e1192d5aca41a5563
refiute <refiute1016@gmail.com>
2017-03-24 22:44:50 +0900

    add terminal-notify for osx and fix ignore_programs

--- notify.zsh
+++ notify.zsh
@@ -1,12 +1,13 @@
 autoload -U add-zsh-hook 2> /dev/null || return
 
 __notify_threshold=60
-read -r -d '' __notify_ignore_commands <<EOS
-less man
+read -r -d '' __notify_ignore_programs <<EOF
+less
+man
 vi vim emacs nano
 git
 ssh
-EOS
+EOF
 
 export __notify_threshold
 export __notify_ignore_programs
@@ -20,9 +21,14 @@ function __notify_precmd() {
 	export __notify_time_end=`date +%s`
 
 	local cmd=$__notify_command
-	local prog=$(echo $cmd|awk '{print $1}')
+	local prog=$(echo $cmd | awk '{print $1}')
+	local notify_method
 
-	if [ -z "$NOTIFY_URL" ]; then
+	if which terminal-notifier >/dev/null 2>&1; then
+		notify_method="terminal-notifier"
+	elif [ -n "$NOTIFY_URL" ]; then
+		notify_method="curl"
+	else
 		return
 	fi
 
@@ -39,18 +45,26 @@ function __notify_precmd() {
 		cmd="<UNKNOWN>"
 	fi
 
-	message="Command finished!\ncommand: $cmd\nexec time: $exec_time"
-	message="payload={'channel': '@refiute', 'username': 'zsh-notify', 'text': '$message', 'icon_emoji': ':robot_face:'}"
+	local message="Command finished!\ncommand: $cmd\nexec time: $exec_time"
 
 	if [ "$exec_time" -ge "$__notify_threshold" ]; then
-		curl -X POST --data-urlencode "$message" $NOTIFY_URL
+		case $notify_method in
+			"terminal-notifier" )
+				echo "$message" | terminal-notifier
+				;;
+			"curl" )
+				local payload="payload={'channel': '@refiute', 'username': 'zsh-notify', 'text': '$message', 'icon_emoji': ':robot_face:'}"
+				curl -X POST --data-urlencode "$payload" $NOTIFY_URL
+				;;
+		esac
 	fi
 
 	unset __notify_time_start
 	unset __notify_command
 }
 
-if which curl >/dev/null 2>&1; then
+if which curl >/dev/null 2>&1 ||
+	which terminal-notifier >/dev/null 2>&1; then
 	add-zsh-hook preexec __notify_preexec
 	add-zsh-hook precmd __notify_precmd
 fi
